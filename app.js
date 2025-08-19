51999a66d97c66e13ce84848c17fe0528fb30067
Claudio Quintana <claudioquintanap@gmail.com>
2025-08-18 21:30:39 -0400

    Actualiza app.js con codigo de inicio

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//* El principal objetivo de este desafío es fortalecer tus habilidades
+//  en lógica de programación. Aquí deberás desarrollar la lógica para resolver
+//  el problema*//
+
+let listaAmigoss = [];
+//let numeroMaximo = length.listaAmigos;
+let numeroGenerado = 0;
+let nombreAmigo = "";
+
+
+
+function asignarTextoelemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+
+}
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (listaAmigoss.includes(nombreAmigo)){
+           limpiarCaja(); 
+           return alert("Este nombre ya fue ingresado");
+        }else{
+            listaAmigoss.push(nombreAmigo);
+            limpiarCaja();
+            return nombreAmigo;
+     }  
+  
+}  
+   console.log (listaAmigoss);    
+
+function limpiarCaja() {
+    document.querySelector("#amigo").value = "";
+    
+}
+
+
+function generarNumerosecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log (numeroGenerado);
+  
+}
+//asignarTextoelemento("listaAmigos", "nombreAmigo");
