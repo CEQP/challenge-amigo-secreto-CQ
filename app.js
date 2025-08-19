4ceb32a2b970a8bebc6e5bcfb1fe2545dacc9688
Claudio Quintana <claudioquintanap@gmail.com>
2025-08-18 23:47:41 -0400

    Actualiza version final

--- app.js
+++ app.js
@@ -3,32 +3,32 @@
 //  el problema*//
 
 let listaAmigoss = [];
-//let numeroMaximo = length.listaAmigos;
 let numeroGenerado = 0;
 let nombreAmigo = "";
+let nombreAmigo2 = "";
 
 
-
-function asignarTextoelemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-
-}
 
 function agregarAmigo(){
-    let nombreAmigo = document.getElementById("amigo").value;
-    if (listaAmigoss.includes(nombreAmigo)){
+    if (document.getElementById("amigo").value == ""){
+        return alert("Debe ingresar un nombre valido");
+    }else{
+
+        let nombreAmigo2 = document.getElementById("amigo").value;
+        nombreAmigo = nombreAmigo2.toUpperCase() //deja todo en MAYUSC para validar siempre el nombre
+     if (listaAmigoss.includes(nombreAmigo)){
            limpiarCaja(); 
            return alert("Este nombre ya fue ingresado");
         }else{
             listaAmigoss.push(nombreAmigo);
+            muestralista();
             limpiarCaja();
             return nombreAmigo;
      }  
+    }
   
 }  
-   console.log (listaAmigoss);    
+ //  console.log (listaAmigoss);    //revisamos como va llenando la lista
 
 function limpiarCaja() {
     document.querySelector("#amigo").value = "";
@@ -36,9 +36,35 @@ function limpiarCaja() {
 }
 
 
-function generarNumerosecreto() {
-    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
-    console.log (numeroGenerado);
+function sortearAmigo() {
+    //document.getElementById("listaAmigos").innerHTML = ""; // limpia lista HTML
+    
+    let numeroGenerado = Math.floor(Math.random()*listaAmigoss.length);
+    //console.log (numeroGenerado);//revisamos el index del amigo en el array
+    const gana = document.getElementById('resultado');
+    const elementoLista = document.createElement('li');
+    elementoLista.textContent = listaAmigoss[numeroGenerado];
+    gana.appendChild(elementoLista);
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.querySelector("#agregaAmi").setAttribute("disabled","true");
+    document.querySelector("#sorteoAmi").setAttribute("disabled","true");
   
 }
-//asignarTextoelemento("listaAmigos", "nombreAmigo");
+
+function condicionesIniciales(){
+    listaAmigoss = [];
+}
+
+
+function muestralista(){
+
+    const lista = document.getElementById('listaAmigos');
+    document.getElementById("listaAmigos").innerHTML = "";
+    
+    listaAmigoss.forEach(function(nombre) {
+    const elementoLista = document.createElement('li');
+    elementoLista.textContent = nombre;
+    lista.appendChild(elementoLista);
+    })
+}    
+
